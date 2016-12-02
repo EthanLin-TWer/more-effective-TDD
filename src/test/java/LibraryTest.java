d067187372c97997d49662f5ec8d6f4cc286a08e
linesh <linesh.simpcity@gmail.com>
2016-12-03 00:43:45 +0800

    Revert "[#1] Add unused imports to failed the travis build, see if badge'll become red"
    This reverts commit eff03b81f869602bd7a1b566ce2e246723b7fad0.

--- src/test/java/LibraryTest.java
+++ src/test/java/LibraryTest.java
@@ -4,7 +4,6 @@ import org.junit.jupiter.api.Test;
 import org.mockito.Mock;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
-import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.MockitoAnnotations.initMocks;
 
