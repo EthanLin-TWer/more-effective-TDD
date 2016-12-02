eff03b81f869602bd7a1b566ce2e246723b7fad0
linesh <linesh.simpcity@gmail.com>
2016-12-03 00:41:11 +0800

    [#1] Add unused imports to failed the travis build, see if badge'll become red

--- src/test/java/LibraryTest.java
+++ src/test/java/LibraryTest.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.mockito.Mock;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.MockitoAnnotations.initMocks;
 
