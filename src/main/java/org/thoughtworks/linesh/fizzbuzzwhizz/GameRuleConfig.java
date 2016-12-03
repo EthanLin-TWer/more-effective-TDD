46adaeaf113838da513f244bcd3be6310c5859ca
linesh <linesh.simpcity@gmail.com>
2016-12-03 22:40:50 +0800

    [#2] Create SpecialNumber class to migrate firstSpecialNumber inside it

--- src/main/java/org/thoughtworks/linesh/fizzbuzzwhizz/GameRuleConfig.java
+++ src/main/java/org/thoughtworks/linesh/fizzbuzzwhizz/GameRuleConfig.java
@@ -1,15 +1,17 @@
 package org.thoughtworks.linesh.fizzbuzzwhizz;
 
 public class GameRuleConfig {
+    // TODO: [Linesh][12/3/16] Checkout REFACTOR to see how to migrate fields to another class
     private int firstSpecialNumber;
     private int secondSpecialNumber;
+    private SpecialNumber specialNumbers = new SpecialNumber();
 
     public void setFirstSpecialNumber(int firstSpecialNumber) {
-        this.firstSpecialNumber = firstSpecialNumber;
+        this.specialNumbers.setFirst(firstSpecialNumber);
     }
 
     public int getFirstSpecialNumber() {
-        return firstSpecialNumber;
+        return this.specialNumbers.getFirst();
     }
 
     public void setSecondSpecialNumber(int secondSpecialNumber) {
---
+++ src/main/java/org/thoughtworks/linesh/fizzbuzzwhizz/SpecialNumber.java
@@ -0,0 +1,13 @@
+package org.thoughtworks.linesh.fizzbuzzwhizz;
+
+public class SpecialNumber {
+    private int first;
+
+    public void setFirst(int first) {
+        this.first = first;
+    }
+
+    public int getFirst() {
+        return first;
+    }
+}
