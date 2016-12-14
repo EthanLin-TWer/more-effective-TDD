d9b0f4fedb3a0281cf8ac7043e927c0dd84cf68b
linesh <linesh.simpcity@gmail.com>
2016-12-14 21:23:16 +0800

    [#2] Estimate tasks time

--- src/test/java/org/thoughtworks/linesh/fizzbuzzwhizz/StudentTest.java
+++ src/test/java/org/thoughtworks/linesh/fizzbuzzwhizz/StudentTest.java
@@ -3,47 +3,47 @@ package org.thoughtworks.linesh.fizzbuzzwhizz;
 import support.Tasking;
 
 class StudentTest {
-    @Tasking
+    @Tasking(estimated = "2mins")
     class should_say_fizz_when_number_can_be_devided_by_the_first_special_number{
 
     }
 
-    @Tasking
+    @Tasking(estimated = "2mins")
     class should_say_buzz_when_number_can_be_devided_by_the_second_special_number {
 
     }
 
-    @Tasking
+    @Tasking(estimated = "2mins")
     class should_say_whizz_when_number_can_be_devided_by_the_third_special_number {
 
     }
 
-    @Tasking
+    @Tasking(estimated = "2mins")
     class should_say_fizzbuzz_when_number_can_be_devided_by_the_first_and_second_special_number {
 
     }
 
-    @Tasking
+    @Tasking(estimated = "2mins")
     class should_say_buzz_whizz_when_number_can_be_devided_by_the_second_and_third_special_number {
 
     }
 
-    @Tasking
+    @Tasking(estimated = "2mins")
     class should_say_fizzwhizz_when_number_can_be_devided_by_the_first_and_third_special_number {
 
     }
 
-    @Tasking
+    @Tasking(estimated = "2mins")
     class should_say_fizzbuzzwhizz_when_number_can_be_devided_by_the_all_three_special_number {
 
     }
 
-    @Tasking
+    @Tasking(estimated = "30mins")
     class should_say_fizz_when_number_contains_the_first_special_number {
 
     }
 
-    @Tasking
+    @Tasking(estimated = "2mins")
     class should_say_number_itself_when_none_of_the_rules_above_applies {
 
     }
