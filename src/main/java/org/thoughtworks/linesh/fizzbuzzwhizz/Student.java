97d71b17307c455e9125adc2ad50d0692e071158
linesh <linesh.simpcity@gmail.com>
2016-12-14 21:36:02 +0800

    [#2] Added two todos

--- src/main/java/org/thoughtworks/linesh/fizzbuzzwhizz/Student.java
+++ src/main/java/org/thoughtworks/linesh/fizzbuzzwhizz/Student.java
@@ -2,6 +2,8 @@ package org.thoughtworks.linesh.fizzbuzzwhizz;
 
 public class Student {
     public String say(int number, int firstSpecial) {
+        // TODO: [Linesh][12/14/16] should be able to pass in 2 special numbers
+        // TODO: [Linesh][12/14/16] remvoe hardcoded 'second' special number 5
         if (firstSpecial == 5) return "Buzz";
         return number % firstSpecial == 0 ? "Fizz" : "";
     }
