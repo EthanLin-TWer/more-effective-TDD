7c9b0c9f5696cf5bdd30e9e88f7057a1cd18b3da
linesh <linesh.simpcity@gmail.com>
2016-12-14 21:29:39 +0800

    [#2] Should say fizz when number is 6 and first special number is 3

--- src/test/java/org/thoughtworks/linesh/fizzbuzzwhizz/StudentTest.java
+++ src/test/java/org/thoughtworks/linesh/fizzbuzzwhizz/StudentTest.java
@@ -1,17 +1,33 @@
 package org.thoughtworks.linesh.fizzbuzzwhizz;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import support.Tasking;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class StudentTest {
+
+    private Student student;
+
+    @BeforeEach
+    void setUp() {
+        student = new Student();
+    }
+
     @Tasking(estimated = "2mins")
     class should_say_fizz_when_number_can_be_devided_by_the_first_special_number {
+
         @Test
         void say_fizz_when_number_is_3() {
-            Student student = new Student();
             String said = student.say(3, 3);
+
+            assertEquals("Fizz", said);
+        }
+
+        @Test
+        void should_say_fizz_when_number_is_6() {
+            String said = student.say(6, 3);
 
             assertEquals("Fizz", said);
         }
