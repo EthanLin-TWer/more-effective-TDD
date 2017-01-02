eef3c731085789ae59fbae2576d6a4c7680cdd21
linesh <linesh.simpcity@gmail.com>
2017-01-02 08:52:52 +0800

    [#3] Add three todos

--- src/test/java/org/thoughtworks/linesh/multicurrencymoney/MoneyTest.java
+++ src/test/java/org/thoughtworks/linesh/multicurrencymoney/MoneyTest.java
@@ -13,7 +13,10 @@ class MoneyTest {
         @Test
         void should_get_10_dollars_when_multiple_2_with_5() {
             Dollar five = new Dollar(5);
+            // TODO: [Linesh][1/2/17] Side effect of times method?
             five.times(2);
+            // TODO: [Linesh][1/2/17] Currency amount as int?
+            // TODO: [Linesh][1/2/17] 'amount' should not be accessed directly, should be private 
             assertEquals(10, five.amount);
         }
     }
