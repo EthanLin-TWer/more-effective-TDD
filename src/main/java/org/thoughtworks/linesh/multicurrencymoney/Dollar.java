6e099f3806a3c37362e6e6234c13ca48b7a0afd8
linesh <linesh.simpcity@gmail.com>
2017-01-02 12:38:09 +0800

    [#3] Refactor, extract type check

--- src/main/java/org/thoughtworks/linesh/multicurrencymoney/Dollar.java
+++ src/main/java/org/thoughtworks/linesh/multicurrencymoney/Dollar.java
@@ -18,8 +18,11 @@ public class Dollar {
     // TODO: [Linesh][1/2/17] Implement equals() method for value object
 
     @Override
-    public boolean equals(Object dollar) {
-        return ((Dollar) dollar).getAmount() == this.getAmount();
+    public boolean equals(Object object) {
+        if (!(object instanceof Dollar)) return false;
+
+        Dollar dollar = (Dollar) object;
+        return dollar.getAmount() == this.getAmount();
     }
 
 
