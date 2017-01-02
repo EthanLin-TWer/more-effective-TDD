00e9a62bbf49c5e06677f26bfa22db9d59c514a3
linesh <linesh.simpcity@gmail.com>
2017-01-02 12:22:23 +0800

    [#3] Remove completed todo

--- src/test/java/org/thoughtworks/linesh/multicurrencymoney/MoneyTest.java
+++ src/test/java/org/thoughtworks/linesh/multicurrencymoney/MoneyTest.java
@@ -13,8 +13,8 @@ class MoneyTest {
         @Test
         void should_get_10_dollars_when_multiple_2_with_5() {
             Dollar five = new Dollar(5);
-            // TODO: [Linesh][1/2/17] Side effect of times method?
             Dollar ten = five.times(2);
+
             // TODO: [Linesh][1/2/17] Currency amount as int?
             assertEquals(5 * 2, ten.getAmount());
         }
