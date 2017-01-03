e3029886127cd594b0b4501fd39fe07d6c3a674f
linesh <linesh.simpcity@gmail.com>
2017-01-03 22:13:24 +0800

    [#3] Refactor, make France extends Money as well, remove amount field

--- src/main/java/org/thoughtworks/linesh/multicurrencymoney/France.java
+++ src/main/java/org/thoughtworks/linesh/multicurrencymoney/France.java
@@ -1,27 +1,20 @@
 package org.thoughtworks.linesh.multicurrencymoney;
 
 // duplicated code of Dollar class and France class
-public class France {
-    private int amount;
-
+public class France extends Money {
     public France(int amount) {
-        this.amount = amount;
+        super(amount);
     }
 
     public France times(int multiplier) {
         return new France(this.amount * multiplier);
     }
 
-    public int getAmount() {
-        // TODO: [Linesh][1/2/17] Currency amount as int?
-        return this.amount;
-    }
-
     // TODO: [Linesh][1/2/17] Implement equality check with null object
     // TODO: [Linesh][1/2/17] Implement equality check with different object type
     @Override
     public boolean equals(Object object) {
-        France france = (France) object;
+        Money france = (Money) object;
         return france.getAmount() == this.getAmount();
     }
 
