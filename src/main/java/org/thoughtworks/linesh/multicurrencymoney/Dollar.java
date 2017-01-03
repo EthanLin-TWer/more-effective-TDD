1c1e0b81793348fb75e84a76e938e0fd69c716d7
linesh <linesh.simpcity@gmail.com>
2017-01-03 22:10:46 +0800

    [#3] Create Money class and pull amount field up in Dollar to Money

--- src/main/java/org/thoughtworks/linesh/multicurrencymoney/Dollar.java
+++ src/main/java/org/thoughtworks/linesh/multicurrencymoney/Dollar.java
@@ -1,26 +1,20 @@
 package org.thoughtworks.linesh.multicurrencymoney;
 
-public class Dollar {
-    private int amount;
+public class Dollar extends Money {
 
     public Dollar(int amount) {
-        this.amount = amount;
+        super(amount);
     }
 
     public Dollar times(int multiplier) {
         return new Dollar(this.amount * multiplier);
     }
 
-    public int getAmount() {
-        // TODO: [Linesh][1/2/17] Currency amount as int?
-        return this.amount;
-    }
-
     // TODO: [Linesh][1/2/17] Implement equality check with different object type
     @Override
     public boolean equals(Object object) {
         if (object == null) return false;
-        Dollar dollar = (Dollar) object;
+        Money dollar = (Money) object;
         return dollar.getAmount() == this.getAmount();
     }
 
