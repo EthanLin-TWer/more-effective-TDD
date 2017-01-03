9c1e0188f5b8655d9c46dd154a60a9ffec17a1d2
linesh <linesh.simpcity@gmail.com>
2017-01-03 22:00:15 +0800

    [#3] Rename france to dollar in Dollar.java, no idea why it had changed

--- src/main/java/org/thoughtworks/linesh/multicurrencymoney/Dollar.java
+++ src/main/java/org/thoughtworks/linesh/multicurrencymoney/Dollar.java
@@ -7,8 +7,8 @@ public class Dollar {
         this.amount = amount;
     }
 
-    public France times(int multiplier) {
-        return new France(this.amount * multiplier);
+    public Dollar times(int multiplier) {
+        return new Dollar(this.amount * multiplier);
     }
 
     public int getAmount() {
@@ -20,8 +20,8 @@ public class Dollar {
     // TODO: [Linesh][1/2/17] Implement equality check with different object type
     @Override
     public boolean equals(Object object) {
-        France france = (France) object;
-        return france.getAmount() == this.getAmount();
+        Dollar dollar = (Dollar) object;
+        return dollar.getAmount() == this.getAmount();
     }
 
 
