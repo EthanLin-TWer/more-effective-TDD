c1f01468a5e03cc5bdf401cd038c15a3cd81cb23
linesh <linesh.simpcity@gmail.com>
2017-01-04 21:51:00 +0800

    [#3] Refactor, pull both equals method up to Money class and remove todo

--- src/main/java/org/thoughtworks/linesh/multicurrencymoney/France.java
+++ src/main/java/org/thoughtworks/linesh/multicurrencymoney/France.java
@@ -9,16 +9,6 @@ public class France extends Money{
     public France times(int multiplier) {
         return new France(this.amount * multiplier);
     }
-
-    @Override
-    public boolean equals(Object object) {
-        // TODO: [Linesh][1/4/17] Check *Refactor* about the steps to pull members up 
-        // TODO: [Linesh][1/2/17] Implement equality check with different object type
-        if (object == null) return false;
-
-        Money money = (Money) object;
-        return money.getAmount() == this.getAmount();
-    }
-
+    
     // TODO: [Linesh][1/2/17] Implement hashCode() method
 }
