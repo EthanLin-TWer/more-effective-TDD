4f918b5f7eb9c1d3d521d887c8d6b76259ad441e
linesh <linesh.simpcity@gmail.com>
2017-01-04 21:43:50 +0800

    [#3] Pull equals method up to Money super class

--- src/main/java/org/thoughtworks/linesh/multicurrencymoney/Dollar.java
+++ src/main/java/org/thoughtworks/linesh/multicurrencymoney/Dollar.java
@@ -10,15 +10,5 @@ public class Dollar extends Money {
         return new Dollar(this.amount * multiplier);
     }
 
-    @Override
-    public boolean equals(Object object) {
-        // TODO: [Linesh][1/4/17] Check *Refactor* about the steps to pull members up 
-        // TODO: [Linesh][1/2/17] Implement equality check with different object type
-        if (object == null) return false;
-        
-        Dollar money = (Dollar) object;
-        return money.getAmount() == this.getAmount();
-    }
-
     // TODO: [Linesh][1/2/17] Implement hashCode() method
 }
--- src/main/java/org/thoughtworks/linesh/multicurrencymoney/Money.java
+++ src/main/java/org/thoughtworks/linesh/multicurrencymoney/Money.java
@@ -11,4 +11,14 @@ public class Money {
         // TODO: [Linesh][1/2/17] Currency amount as int?
         return this.amount;
     }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: [Linesh][1/4/17] Check *Refactor* about the steps to pull members up 
+        // TODO: [Linesh][1/2/17] Implement equality check with different object type
+        if (object == null) return false;
+        
+        Dollar money = (Dollar) object;
+        return money.getAmount() == this.getAmount();
+    }
 }
