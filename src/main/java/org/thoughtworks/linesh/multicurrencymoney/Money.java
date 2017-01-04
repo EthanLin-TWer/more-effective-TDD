e18221630e300b502ab624294ad9d7fba73d7194
linesh <linesh.simpcity@gmail.com>
2017-01-04 08:39:29 +0800

    [#3] Add todo: To check how to pull members up

--- src/main/java/org/thoughtworks/linesh/multicurrencymoney/Money.java
+++ src/main/java/org/thoughtworks/linesh/multicurrencymoney/Money.java
@@ -14,8 +14,10 @@ public class Money {
 
     @Override
     public boolean equals(Object object) {
+        // TODO: [Linesh][1/4/17] Check *Refactor* about the steps to pull members up 
         // TODO: [Linesh][1/2/17] Implement equality check with different object type
         if (object == null) return false;
+        
         Money money = (Money) object;
         return money.getAmount() == this.getAmount();
     }
