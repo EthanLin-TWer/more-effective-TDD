b69294320e05cbb486b040316cc36bee5b2b67df
linesh <linesh.simpcity@gmail.com>
2017-01-04 21:40:23 +0800

    [#3] Revert money base class extraction, prepare for pull member up practice

--- src/main/java/org/thoughtworks/linesh/multicurrencymoney/Dollar.java
+++ src/main/java/org/thoughtworks/linesh/multicurrencymoney/Dollar.java
@@ -1,13 +1,30 @@
 package org.thoughtworks.linesh.multicurrencymoney;
 
-public class Dollar extends Money {
+public class Dollar {
+
+    protected int amount;
 
     public Dollar(int amount) {
-        super(amount);
+        this.amount = amount;
     }
 
     public Dollar times(int multiplier) {
         return new Dollar(this.amount * multiplier);
+    }
+
+    public int getAmount() {
+        // TODO: [Linesh][1/2/17] Currency amount as int?
+        return this.amount;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: [Linesh][1/4/17] Check *Refactor* about the steps to pull members up 
+        // TODO: [Linesh][1/2/17] Implement equality check with different object type
+        if (object == null) return false;
+        
+        Dollar money = (Dollar) object;
+        return money.getAmount() == this.getAmount();
     }
 
     // TODO: [Linesh][1/2/17] Implement hashCode() method
--- src/main/java/org/thoughtworks/linesh/multicurrencymoney/France.java
+++ src/main/java/org/thoughtworks/linesh/multicurrencymoney/France.java
@@ -1,13 +1,28 @@
 package org.thoughtworks.linesh.multicurrencymoney;
 
 // duplicated code of Dollar class and France class
-public class France extends Money {
+public class France {
+    private int amount;
     public France(int amount) {
-        super(amount);
+        this.amount = amount;
+    }
+
+    public int getAmount() {
+        return this.amount;
     }
 
     public France times(int multiplier) {
         return new France(this.amount * multiplier);
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: [Linesh][1/4/17] Check *Refactor* about the steps to pull members up 
+        // TODO: [Linesh][1/2/17] Implement equality check with different object type
+        if (object == null) return false;
+
+        France france = (France) object;
+        return france.getAmount() == this.getAmount();
     }
 
     // TODO: [Linesh][1/2/17] Implement hashCode() method
