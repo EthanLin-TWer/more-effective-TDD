dbc0373cb7b4192492545e4afd09391ebe18c109
linesh <linesh.simpcity@gmail.com>
2017-01-04 22:03:34 +0800

    [#3] Refactor, change all reference point of new Dollar() to Money.dollar()

--- src/test/java/org/thoughtworks/linesh/multicurrencymoney/DollarTest.java
+++ src/test/java/org/thoughtworks/linesh/multicurrencymoney/DollarTest.java
@@ -14,36 +14,36 @@ class DollarTest {
             Dollar five = Money.dollar(5);
 
             // It's actually a risk here that we rely on equality function for tests to work
-            assertEquals(new Dollar(10), five.times(2));
+            assertEquals(Money.dollar(10), five.times(2));
         }
 
         @Test
         void should_get_15_dollars_when_multiple_3_with_5() {
-            Dollar five = new Dollar(5);
+            Dollar five = Money.dollar(5);
 
-            assertEquals(new Dollar(15), five.times(3));
+            assertEquals(Money.dollar(15), five.times(3));
         }
 
         @Tasking @Nested
         class should_test_equality {
             @Test
             void should_5_dollars_equals_to_5_dollars() {
-                assertTrue(new Dollar(5).equals(new Dollar(5)));
+                assertTrue(Money.dollar(5).equals(Money.dollar(5)));
             }
 
             @Test
             void should_5_dollars_not_equals_to_6_dollars() {
-                assertFalse(new Dollar(5).equals(new Dollar(6)));
+                assertFalse(Money.dollar(5).equals(Money.dollar(6)));
             }
 
             @Test
             void should_5_dollars_not_equals_to_null() {
-                assertFalse(new Dollar(5).equals(null));
+                assertFalse(Money.dollar(5).equals(null));
             }
 
             @Test
             void should_5_dollars_not_equals_to_5_france() {
-                assertFalse(new Dollar(5).equals(new France(5)));
+                assertFalse(Money.dollar(5).equals(new France(5)));
             }
         }
     }
