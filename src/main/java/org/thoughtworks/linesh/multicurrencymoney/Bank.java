fa8462d4d14223bf6c7371e03223bf8c8a992d49
linesh <linesh.simpcity@gmail.com>
2017-01-09 23:07:25 +0800

    [#3] Wording, refine todo

--- src/main/java/org/thoughtworks/linesh/multicurrencymoney/Bank.java
+++ src/main/java/org/thoughtworks/linesh/multicurrencymoney/Bank.java
@@ -2,7 +2,7 @@ package org.thoughtworks.linesh.multicurrencymoney;
 
 public class Bank {
     public Money reduced(Expression money, String toCurrency) {
-        // TODO: [Linesh][1/9/17] Type cast is a bad smell? 
+        // TODO: [Linesh][1/9/17] Is type cast a bad smell? 
         Sum sum = (Sum) money;
         return sum.reduced(money, toCurrency);
     }
