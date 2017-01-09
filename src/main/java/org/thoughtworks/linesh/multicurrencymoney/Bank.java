9fb3b1bfee5d166bfca2b929a847e2f9ea3194c3
linesh <linesh.simpcity@gmail.com>
2017-01-09 23:02:33 +0800

    [#3] Add three todos

--- src/main/java/org/thoughtworks/linesh/multicurrencymoney/Bank.java
+++ src/main/java/org/thoughtworks/linesh/multicurrencymoney/Bank.java
@@ -2,8 +2,11 @@ package org.thoughtworks.linesh.multicurrencymoney;
 
 public class Bank {
     public Money reduced(Expression money, String toCurrency) {
+        // TODO: [Linesh][1/9/17] Type cast is a bad smell? 
         Sum sum = (Sum) money;
+        // TODO: [Linesh][1/9/17] Second chained call
         int amount = sum.getAddend().getAmount() + sum.getAugend().getAmount();
+        // TODO: [Linesh][1/9/17] Currency is not used 
         return Money.dollar(amount);
     }
 }
