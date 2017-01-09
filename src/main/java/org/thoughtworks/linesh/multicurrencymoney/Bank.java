f1b5532c575a22821689202863cd1194f1692e81
linesh <linesh.simpcity@gmail.com>
2017-01-09 23:18:51 +0800

    [#3] Refactor, remove casts and use polymorphism to impl reduced() instead

--- src/main/java/org/thoughtworks/linesh/multicurrencymoney/Bank.java
+++ src/main/java/org/thoughtworks/linesh/multicurrencymoney/Bank.java
@@ -2,9 +2,6 @@ package org.thoughtworks.linesh.multicurrencymoney;
 
 public class Bank {
     public Money reduced(Expression money, String toCurrency) {
-        // TODO: [Linesh][1/9/17] Is type cast a bad smell? 
-        if (money instanceof Money) return money.reduced(toCurrency);
-        Sum sum = (Sum) money;
-        return sum.reduced(toCurrency);
+        return money.reduced(toCurrency);
     }
 }
