9081fe077ac172be8d1b170bde3ae86c1543a6e0
linesh <linesh.simpcity@gmail.com>
2017-01-09 23:14:33 +0800

    [#3] Remove no used parameter

--- src/main/java/org/thoughtworks/linesh/multicurrencymoney/Bank.java
+++ src/main/java/org/thoughtworks/linesh/multicurrencymoney/Bank.java
@@ -4,8 +4,7 @@ public class Bank {
     public Money reduced(Expression money, String toCurrency) {
         // TODO: [Linesh][1/9/17] Is type cast a bad smell? 
         if (money instanceof Money) return (Money) money;
-        
         Sum sum = (Sum) money;
-        return sum.reduced(money, toCurrency);
+        return sum.reduced(toCurrency);
     }
 }
