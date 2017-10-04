0f87c22d7f4b25fcc07a1c0e688a1833eea3b377
Linesh <linesh.simpcity@gmail.com>
2017-10-05 01:35:29 +0800

    [Linesh] 🐛Fix: fix failing eslint

--- test/index.test.js
+++ test/index.test.js
@@ -2,6 +2,6 @@ import assert from 'assert'
 
 describe('first test', () => {
   it('should return 2 when 1 + 1', () => {
-    assert.equal(1 + 30 / 3 * 2, 21)
+    assert.equal(1 + 30, 31)
   })
 })
