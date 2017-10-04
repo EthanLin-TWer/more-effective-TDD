1af2732a5fa79387dc34c032abb872cf02cceeee
Linesh <linesh.simpcity@gmail.com>
2017-10-04 23:14:15 +0800

    [Linesh] 🔧Chore: removing all original Java facilities, shifting to JS

--- src/main/java/tdd/howto/linesh/tw/multicurrencymoney/Bank.java
+++
@@ -1,7 +0,0 @@
-package tdd.howto.linesh.tw.multicurrencymoney;
-
-public class Bank {
-    public Money reduced(Expression money, String toCurrency) {
-        return money.reduce(toCurrency);
-    }
-}
--- src/main/java/tdd/howto/linesh/tw/multicurrencymoney/Expression.java
+++
@@ -1,7 +0,0 @@
-package tdd.howto.linesh.tw.multicurrencymoney;
-
-public interface Expression {
-    Expression plus(Money money);
-
-    Money reduce(String currency);
-}
