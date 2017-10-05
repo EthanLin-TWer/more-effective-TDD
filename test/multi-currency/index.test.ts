b8f4841dc843bb0c2ee56fe917d7c2d708b6a942
Linesh <linesh.simpcity@gmail.com>
2017-10-06 02:22:44 +0800

    [Linesh] ♻️Refactor: tests

--- test/multi-currency/index.test.ts
+++ test/multi-currency/index.test.ts
@@ -2,22 +2,12 @@ import * as assert from 'assert'
 import Dollar from '../../src/multi-currency'
 
 it('multiplication', () => {
-  const five = new Dollar(5)
-
-  assert(five.times(2).equals(new Dollar(10)))
-  assert(five.times(3).equals(new Dollar(15)))
+  assert(new Dollar(5).times(2).equals(new Dollar(10)))
+  assert(new Dollar(5).times(3).equals(new Dollar(15)))
 })
 
-describe('money equality', () => {
-  it('should return true when asking if $5 equals $5', () => {
-    assert.equal(new Dollar(5).equals(new Dollar(5)), true)
-  })
-
-  it('should return false when asking if $5 equals $4', () => {
-    assert.equal(new Dollar(5).equals(new Dollar(4)), false)
-  })
-
-  it('should return false when comparing with null money', () => {
-    assert.equal(new Dollar(5).equals(null), false)
-  })
+it('money equality', () => {
+  assert.equal(new Dollar(5).equals(new Dollar(5)), true)
+  assert.equal(new Dollar(5).equals(new Dollar(4)), false)
+  assert.equal(new Dollar(5).equals(null), false)
 })
