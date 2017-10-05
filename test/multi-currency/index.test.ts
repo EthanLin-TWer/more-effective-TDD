bbcfd5443d30badfe712c5deab332eba4fbf7c5b
Linesh <linesh.simpcity@gmail.com>
2017-10-06 01:47:41 +0800

    [Linesh] ♻️Refactor: make amount equality explicit after equals() is written

--- test/multi-currency/index.test.ts
+++ test/multi-currency/index.test.ts
@@ -4,16 +4,16 @@ import Dollar from '../../src/multi-currency'
 describe('multiplication', () => {
   it('should return 10 when 5 times 2', () => {
     const five = new Dollar(5)
-    const result = five.times(2)
+    const product = five.times(2)
 
-    assert.equal(10, result.amount)
+    assert(product.equals(new Dollar(10)))
   })
 
   it('should return 15 when 5 times 3', () => {
     const five = new Dollar(5)
-    const result = five.times(3)
+    const product = five.times(3)
 
-    assert.equal(15, result.amount)
+    assert(product.equals(new Dollar(15)))
   })
 })
 
